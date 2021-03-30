b7bac17c8d22cef51d284cb8ea639073e33a27f0
Mads Balslev <madspbalslev@gmail.com>
2021-03-30 12:02:40 +0200

    Jsdoc mads (#9)
    * Use JSDoc
    
    * Use JSDoc
    
    * Add workflow
    
    * small change

--- server/src/app.js
+++ server/src/app.js
@@ -3,88 +3,86 @@ const http = require('http');
 const path = require('path');
 const url = require('url');
 const fs = require('fs');
- 
-const server = http.createServer(); // <----- real port should go here
+
+const server = http.createServer();
 
 const filePath = path.join(__dirname, '../public/site.html');
 
 server.on('request', tryHandleRequest);
 
+/** This is shit */
 function tryHandleRequest(request, response) {
-    try {
-        handleRequest(request, response);
-    } catch (error) {
-        errorResponse(response, 400, error);
-    }
+  try {
+    handleRequest(request, response);
+  } catch (error) {
+    errorResponse(response, 400, error);
+  }
 }
 
 function handleRequest(request, response) {
-    switch (request.url) {
-        case '/':
-            handleBaseRequest(request, response);
-            break;
+  switch (request.url) {
+    case '/':
+      handleBaseRequest(request, response);
+      break;
 
-        case '/opgaver':  
-            handleOpgaverRequest(request, response);
-            break;
-        
-        case '/resultat':  
-            handleResultatRequest(request, response);
-            break;
+    case '/opgaver':
+      handleOpgaverRequest(request, response);
+      break;
 
-        default:
-            throw 'bad path';
-            break;
-    }
+    case '/resultat':
+      handleResultatRequest(request, response);
+      break;
+
+    default:
+      throw 'bad path';
+  }
 }
 
 function handleBaseRequest(request, response) {
-    if (request.method === 'GET') {
-        respondWith(filePath, response);
-        console.log(request.headers['x-forwarded-for'] || request.connection.remoteAddress);
-    } else if (request.method !== 'GET') {
-        throw 'bad request';
-    }
+  if (request.method === 'GET') {
+    respondWith(filePath, response);
+  } else if (request.method !== 'GET') {
+    throw 'bad request';
+  }
 }
 
 function handleOpgaverRequest(request, response) {
-    if (request.method === 'GET') {
-        // hent opgave fra db
-    } else if (request.method === 'POST') {
-        // compare answer to assignment id
-    } else if (request.method !== 'GET' && request.method !== 'POST') {
-        throw 'bad request'
-    }
+  if (request.method === 'GET') {
+    // hent opgave fra db
+  } else if (request.method === 'POST') {
+    // compare answer to assignment id
+  } else if (request.method !== 'GET' && request.method !== 'POST') {
+    throw 'bad request';
+  }
 }
 
 function handleResultatRequest(request, response) {
-    if (request.method === 'GET') {
-        respondWith(filePath, response);
-        console.log(request.headers['x-forwarded-for'] || request.connection.remoteAddress);
-    } else if (request.method !== 'GET') {
-        throw 'bad request';
-    }
+  if (request.method === 'GET') {
+    respondWith(filePath, response);
+    console.log(request.headers['x-forwarded-for'] || request.connection.remoteAddress);
+  } else if (request.method !== 'GET') {
+    throw 'bad request';
+  }
 }
 
 function errorResponse(response, code, reason) {
-    response.writeHead(400, {'Content-Type': 'text/txt'});
-    response.write(reason);
-    response.end("\n");
+  response.writeHead(400, { 'Content-Type': 'text/txt' });
+  response.write(reason);
+  response.end('\n');
 }
 
-
 function respondWith(file, response) {
-    fs.readFile(file, function(err, html){
-        if (err){
-            throw err;
-        }
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    });
+  fs.readFile(file, (err, html) => {
+    if (err) {
+      throw err;
+    }
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(html);
+    response.end();
+  });
 }
 
 server.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
-    console.log(filePath);
+  console.log(`Listening on port ${process.env.PORT}`);
+  console.log(filePath);
 });
