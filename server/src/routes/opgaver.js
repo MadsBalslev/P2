6023a6e0c832af909886c80caa1ceb90d627e182
Casper Ståhl <cstahl20@student.aau.dk>
2021-04-06 10:56:05 +0200

    refactor of /src

---
+++ server/src/routes/opgaver.js
@@ -0,0 +1,16 @@
+/**
+ * Will handle requests to the '/opgaver' route
+ * @param {*} request
+ * @param {*} response
+ */
+function handleOpgaverRequest(request, response) {
+  if (request.method === 'GET') {
+    // hent opgave fra db
+  } else if (request.method === 'POST') {
+    // compare answer to assignment id
+  } else if (request.method !== 'GET' && request.method !== 'POST') {
+    throw 'bad request';
+  }
+}
+
+
