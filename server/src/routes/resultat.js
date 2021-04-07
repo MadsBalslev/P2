7243aa61ee0b14a3edae7f2cbdfb619b0b7bf649
MadsBalslev <MadsBalslev@users.noreply.github.com>
2021-04-07 08:15:45 +0000

    deploy: 67fe3bfcb5f941a5ffa68fa61d5c333d297cbc66

---
+++ server/src/routes/resultat.js
@@ -0,0 +1,19 @@
+/**
+ * Will handle requests to the '/resultater' route
+ * @param {*} request
+ * @param {*} response
+ */
+function handleResultatRequest(request, response) {
+  if (request.method === 'POST') {
+    handleResultatPostRequest(request, response);
+    console.log(request.headers['x-forwarded-for'] || request.connection.remoteAddress);
+  } else if (request.method !== 'POST') {
+    throw 'bad request';
+  }
+}
+
+function handleResultatPostRequest(request, response) {
+  let lint;
+}
+
+module.exports = handleResultatRequest;
