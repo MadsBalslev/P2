4b17057b7b0529ba16aaf8f40338e076bb751f20
Casper Ståhl <cstahl20@student.aau.dk>
2021-04-07 14:55:38 +0200

    add validation of POST /resultat

--- server/src/routes/resultat.js
+++ server/src/routes/resultat.js
@@ -3,17 +3,54 @@
  * @param {*} request
  * @param {*} response
  */
-function handleResultatRequest(request, response) {
+const handleResultatRequest = (request, response) => {
   if (request.method === 'POST') {
     handleResultatPostRequest(request, response);
     console.log(request.headers['x-forwarded-for'] || request.connection.remoteAddress);
   } else if (request.method !== 'POST') {
-    throw 'bad request';
+    throw 'handleResultatRequest did not get a POST request';
   }
-}
+};
 
-function handleResultatPostRequest(request, response) {
-  let lint;
-}
+const handleResultatPostRequest = (request, response) => {
+  let body = '';
+  request.setEncoding('utf8');
+  request.on('data', (chunk) => {
+    body += chunk;
+  });
+
+  request.on('end', () => {
+    body = JSON.parse(body);
+    console.log('got JSON object:');
+    console.log(body);
+    console.log('\n');
+
+    if (requestBodyIsValid(body)) {
+      console.log('was valid');
+      handleRequestBody(body, response);
+    } else if (!(requestBodyIsValid(body))) {
+      response.writeHead(400, { 'Content-Type': 'text/txt' });
+      response.write('handleResultatPostRequest did not get valid body');
+      response.end('\n');
+    }
+  });
+};
+
+const requestBodyIsValid = (body) => {
+  let bodyIsValid = true;
+  for (const opgave of body) {
+    if (!(opgave.hasOwnProperty('id') && opgave.hasOwnProperty('actualAnswer'))) {
+      bodyIsValid = false;
+    }
+    if (typeof opgave.id !== 'number' && typeof opgave.actualAnswer !== 'string') {
+      bodyIsValid = false;
+    }
+  }
+  return bodyIsValid;
+};
+
+const handleRequestBody = (requestBody, response) => {
+
+};
 
 module.exports = handleResultatRequest;
