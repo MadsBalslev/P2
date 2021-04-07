90c8db6dfe3b016545826fbe42f5c4c1bc20670e
MadsBalslev <MadsBalslev@users.noreply.github.com>
2021-04-07 08:08:37 +0000

    deploy: 155a1af602e96f14aa1d1416426609989e3ad192

---
+++ server/src/app.js
@@ -0,0 +1,98 @@
+require('dotenv').config();
+const http = require('http');
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
+const handleOpgaverRequest = require('./routes/opgaver');
+const handleResultatRequest = require('./routes/resultat');
+const helper = require('./helper');
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer();
+
+const filePath = path.join(__dirname, '../public/site.html');
+
+server.on('request', tryHandleRequest);
+
+/**
+ * Will try to handle the request in {@link handleRequest} or if necessary catch the error
+ * @param {*} request
+ * @param {*} response
+ */
+function tryHandleRequest(request, response) {
+  try {
+    console.log('NEW ' + request.method + ' REQUEST:');
+    console.log(request.headers);
+    handleRequest(request, response);
+  } catch (error) {
+    errorResponse(response, 400, error);
+  }
+}
+
+/**
+ * Will check which route is requested and use the correct handler function. If a non-existing route
+ * is requsted it will throw an error which will be catched in {@link tryHandleRequest}
+ * @param {*} request
+ * @param {*} response
+ */
+function handleRequest(request, response) {
+  switch (request.url) {
+    case '/':
+      handleBaseRequest(request, response);
+      break;
+
+    case '/opgaver':
+      handleOpgaverRequest(request, response);
+      break;
+
+    case '/resultat':
+      handleResultatRequest(request, response);
+      break;
+
+    default:
+      throw 'bad path';
+  }
+}
+
+/**
+ * Will handle requests to the '/' route
+ * @param {*} request
+ * @param {*} response
+ */
+function handleBaseRequest(request, response) {
+  if (request.method === 'GET') {
+    respondWith(filePath, 'text/html', response);
+  } else if (request.method !== 'GET') {
+    throw 'bad request';
+  }
+}
+
+/**
+ * Will handle potential errors catched in {@link tryHandleRequest}
+ * @param {*} response
+ * @param {integer} code the http status code
+ * @param {string} reason the error message
+ */
+function errorResponse(response, code, reason) {
+  response.writeHead(code, { 'Content-Type': 'text/txt' });
+  response.write(reason);
+  response.end('\n');
+}
+
+/**
+ * Will respond with a HTML file provided
+ * @param {string} file Path to the HTML file to respond with
+ * @param {string} fileType type of file
+ * @param {*} response
+ */
+async function respondWith(file, fileType, response) {
+  const fileData = await fs.promises.readFile(file);
+  response.writeHead(200, { 'Content-Type': fileType });
+  response.end(fileData);
+}
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+  console.log(filePath);
+});
