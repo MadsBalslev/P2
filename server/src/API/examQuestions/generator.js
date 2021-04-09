8ac56077127f8eefbf7724af3165f2435162cc1c
Mads Balslev <madspbalslev@gmail.com>
2021-04-09 13:26:13 +0200

    Fix mistake

--- server/src/API/examQuestions/generator.js
+++ server/src/API/examQuestions/generator.js
@@ -1,12 +1,8 @@
 const math = require('mathjs');
 const mysql = require('mysql');
+const { dbConnection } = require('../../helper');
 
-const con = mysql.createConnection({
-  host: process.env.DB_HOST || '127.0.0.1',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB || 'test',
-});
+const con = mysql.createConnection(dbConnection);
 
 con.connect((err) => {
   if (err) {
