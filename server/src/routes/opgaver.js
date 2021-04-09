d6d2e9aca07044fab81cd21ac2e6f0f135a138a6
Mads Balslev <madspbalslev@gmail.com>
2021-04-09 14:32:12 +0200

    Server now works

--- server/src/routes/opgaver.js
+++ server/src/routes/opgaver.js
@@ -7,13 +7,15 @@ const getJSON = require('../API/examQuestions/jsonGen');
  */
 const handleOpgaverRequest = async (request, response) => {
   if (request.method === 'GET') {
+    response.setHeader('Access-Control-Allow-Origin', '*');
     // hent opgave fra db
     const result = await getJSON.getJSON();
-    console.log(result);
     response.end(JSON.stringify(result));
   } else if (request.method === 'POST') {
+    response.setHeader('Access-Control-Allow-Origin', '*');
     // compare answer to assignment id
   } else if (request.method !== 'GET' && request.method !== 'POST') {
+    response.setHeader('Access-Control-Allow-Origin', '*');
     throw 'bad request';
   }
 };
