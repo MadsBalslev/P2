12444a2afb9ce0f2dbf7de16eb9868a354b30a48
uglenDX <uglenDX@users.noreply.github.com>
2021-04-09 11:28:47 +0000

    deploy: 0a081ee543a5bf0a68a96287314275c2afde57dd

--- server/src/API/examQuestions/jsonGen.js
+++ server/src/API/examQuestions/jsonGen.js
@@ -1,11 +1,7 @@
 const mysql = require('mysql');
+const { dbConnection } = require('../../helper');
 
-const con = mysql.createConnection({
-  host: process.env.DB_HOST || '127.0.0.1',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'test',
-});
+const con = mysql.createConnection(dbConnection);
 
 con.connect((err) => {
   if (err) {
@@ -14,7 +10,23 @@ con.connect((err) => {
   console.log('Connected!');
 });
 
+const createTable = `CREATE TABLE IF NOT EXISTS examquestions (
+  id int NOT NULL AUTO_INCREMENT,
+  tekst varchar(255) NOT NULL COLLATE utf8_danish_ci,
+  var1 varchar(255) NOT NULL COLLATE utf8_danish_ci,
+  udtryk varchar(3) NOT NULL COLLATE utf8_danish_ci,
+  var2 varchar(255) NOT NULL COLLATE utf8_danish_ci,
+  facit varchar(255) NOT NULL COLLATE utf8_danish_ci,
+  point int NOT NULL,
+  PRIMARY KEY (id)
+);`;
+
 const getExamQuestions = () => new Promise((resolve, reject) => {
+  // Check if table exits and create if not
+  con.query(createTable, (err) => {
+    if (err) console.log(err);
+  });
+
   con.query('SELECT * FROM examquestions', (err, result) => {
     if (!err) resolve(JSON.parse(JSON.stringify(result))); // Hacky solution
     else reject(err);
--- server/src/helper.js
+++ server/src/helper.js
@@ -1,1 +1,12 @@
 // Helper functions should go here.
+
+const dbConnection = {
+  host: process.env.DB_HOST || '127.0.0.1',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'test',
+};
+
+module.exports = {
+  dbConnection,
+};
