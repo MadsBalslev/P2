d1fb84b98db1cef82db95247ec721f2d7270396a
Patrick Bertelsen <pberte20@users.noreply.github.com>
2021-04-13 14:11:16 +0200

    QuestionMetaData now works
    Each questions, id, number and user answer can now be stored in a object

--- server/public/createquestion.js
+++ server/public/createquestion.js
@@ -4,60 +4,63 @@ document.body.onload = Run;
 
 
 
-function Run(){
-    questionNumber = 1;
+function Run() {
+    const questionNumber = 1;
     let questionAmount = 0;
-    let questionMetaData = [];
+    let questionMetaData = {}
     testAnswers = {};
     const form = document.createElement("form");
-    questionMetaData[questionAmount] = createQuestion(0, "Hvad er 2 + 2",4,0);
+    questionMetaData[questionAmount] = createQuestion(0, "Hvad er 2 + 2", 4, 0);
     questionAmount++;
-    questionMetaData[questionAmount] = createQuestion(1, "Hvad er 3 + 3",6,1);
+    questionMetaData[questionAmount] = createQuestion(1, "Hvad er 3 + 3", 6, 1);
     questionAmount++;
     const div = document.createElement("div");
-    let validation =  document.createElement("P")
-    validation.setAttribute("ID","validation");
+    let validation = document.createElement("P")
+    validation.setAttribute("ID", "validation");
     let submitButton = document.createElement("button");
     submitButton.innerHTML = "Send answers"
-    submitButton.onclick = function(){
-        questionAnswers = getAnswers(questionAmount);
+    submitButton.onclick = function () {
+        let questionAnswers = getAnswers(questionAmount);
+        questionMetaData[0].questionanswer = questionAnswers[0];
+        console.log(questionMetaData);
         console.log(questionAnswers);
     }
     div.appendChild(submitButton);
     div.appendChild(validation)
     document.body.appendChild(div);
     console.log(questionMetaData);
-    }
+}
 
 //Creates a div containing elements for a single question
-function createQuestion(questionnumber,questiontext,questionid){
-    {
-        const div = document.createElement("div");
-        let question = document.createElement("p");
-        let textfield = document.createElement("INPUT");
-        textfield.setAttribute("type", "number");
-        textfield.setAttribute("value", "Indtast svar her");
-        textfield.setAttribute("required", "1");
-        div.appendChild(question);
-        div.appendChild(textfield);
-        document.body.appendChild(div);
-        document.getElementsByTagName("p")[questionnumber].innerHTML = questiontext;
-        let questionMetaData = {
-            questionnumber: questionnumber,
-            questionid: questionid,
-        }
-        return questionMetaData;
-        
+function createQuestion(questionnumber, questiontext, questionid) {
+
+    const div = document.createElement("div");
+    let question = document.createElement("p");
+    let textfield = document.createElement("INPUT");
+    textfield.setAttribute("type", "number");
+    textfield.setAttribute("value", "Indtast svar her");
+    textfield.setAttribute("required", "1");
+    div.appendChild(question);
+    div.appendChild(textfield);
+    document.body.appendChild(div);
+    document.getElementsByTagName("p")[questionnumber].innerHTML = questiontext;
+    let questionMetaData = {
+        questionnumber: questionnumber,
+        questionid: questionid,
+        questionanswer: 1,
     }
+    return questionMetaData;
+
+
 }
 // Gets the input from the inputfields and returns them as an array containing all answers
-function getAnswers(questionAmount){
+function getAnswers(questionAmount) {
     i = 0;
     questionAnswers = [];
-    while (i<questionAmount){
+    while (i < questionAmount) {
         //Checks if inputfield is answered and has valid data
         let inputObject = document.getElementsByTagName("input")[i];
-        if (!inputObject.checkValidity()){
+        if (!inputObject.checkValidity()) {
             document.getElementById("validation").innerHTML = ("Du mangler at svare på et eller flere spørgsmål");
             return 0
         }
