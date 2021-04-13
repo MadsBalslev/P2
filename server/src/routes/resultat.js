ee4f7d2f86ca969feacedccd0beb714bbf76ce0b
Casper Ståhl <cstahl20@student.aau.dk>
2021-04-13 11:02:17 +0200

    Revert "Revert "refactor""
    This reverts commit 7f602d2b4596f62678770c99a4d6c16bf3272d67.

--- server/src/routes/resultat.js
+++ server/src/routes/resultat.js
@@ -1,5 +1,114 @@
 const helper = require('../helper');
 
+function ExerciseSuite(exercises) {
+  this.exercises = exercises;
+}
+
+ExerciseSuite.prototype.getExercisesDataFromDatabase = async function getExercisesDataFromDatabase() {
+};
+
+ExerciseSuite.prototype.evaluateAnswersData = function evaluateAnswersData() {
+  this.exercises.forEach((exercise) => {
+    exercise.evaluateSingleAnswer();
+  });
+};
+
+function Exercise(answer) {
+  this.id = answer.id;
+  this.number = answer.questionNumber;
+  this.actualAnswer = answer.actualAnswer;
+  this.answterWasCorrect = undefined;
+  this.tekst = undefined;
+  this.facit = undefined;
+  this.point = undefined;
+  this.type = undefined;
+}
+
+Exercise.prototype.getSingleExerciseDataFromDatabase = function getSingleExerciseDataFromDatabase() {
+};
+
+Exercise.prototype.evaluateSingleAnswer = function evaluateSingleAnswer() {
+  if (this.actualAnswer === this.facit) {
+    this.answerWasCorrect = true;
+  } else if (this.actualAnswer !== this.facit) {
+    this.answerWasCorrect = false;
+  }
+};
+
+function ResultPage(exerciseSuite) {
+  this.page += createPageHead();
+  this.page += createPageBody(exerciseSuite);
+}
+
+ResultPage.prototype.respondTo = function respondTo(response) {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.end(this.page);
+};
+
+const createPageHead = () => `<!DOCTYPE html>
+  <head>
+  <title>P2 Projekt Resultat Side</title>
+  <link rel='icon' type='image/png' href='./favicon.png' />
+  <meta charset="UTF-8">
+  </head>
+  `;
+
+const createPageBody = (exerciseSuite) => {
+  let pageBody = '';
+  pageBody += '<body>';
+
+  exerciseSuite.exercises.forEach((exercise) => {
+    pageBody += createPageBodyExercise(exercise);
+  });
+
+  pageBody += '</body>';
+  return pageBody;
+};
+
+function createPageBodyExercise(exercise) {
+  let pageBody = '';
+  pageBody += `<div class="exercise" id="exercise${exercise.number}">\n`;
+  pageBody += `<h1>Opgave ${exercise.number}</h1>\n`;
+  pageBody += createPageBodyExerciseBody(exercise);
+  pageBody += createPageBodyAnswerBody(exercise);
+  pageBody += createPageBodyExerciseData(exercise);
+  return pageBody;
+}
+
+function createPageBodyExerciseData(exercise) {
+  let pageBody = '';
+  pageBody += '<div class="exerciseData">\n';
+  pageBody += '<h2>Opgave indsigt</h2>\n';
+  pageBody += `<p><b>Emne:</b> ${exercise.type}</p>\n`;
+  pageBody += `<p><b>Points:</b> ${exercise.point}</p>\n`;
+  pageBody += '</div>\n';
+  pageBody += '</div>\n';
+  return pageBody;
+}
+
+function createPageBodyAnswerBody(exercise) {
+  let pageBody = '';
+  pageBody += '<div class="answerBody">\n';
+  pageBody += '<h2> Svar og facit</h2>\n';
+  if (exercise.answerWasCorrect) {
+    pageBody += '<p style="background-color: lightgreen">Dit svar var korrekt</p>\n';
+  } else if (!exercise.answerWasCorrect) {
+    pageBody += '<p style="background-color: lightcoral">Dit svar var forkert</p>\n';
+  }
+  pageBody += `<p><b>Korrekte svar:</b> ${exercise.facit}</p>\n`;
+  pageBody += `<p><b>Dit svar:</b> ${exercise.actualAnswer}</p>\n`;
+  pageBody += '</div>\n';
+  return pageBody;
+}
+
+function createPageBodyExerciseBody(exercise) {
+  let pageBody = '';
+  pageBody += '<div class="exerciseBody">\n';
+  pageBody += `<p>${exercise.tekst}</p>\n`;
+  pageBody += '</div>\n';
+  return pageBody;
+}
+
 /**
  * Will handle requests to the '/result' route
  * @param {*} request
@@ -107,96 +216,5 @@ function convertActualAnswersToExercises(actualAnswers) {
 
   return exercises;
 }
-
-function ExerciseSuite(exercises) {
-  this.exercises = exercises;
-
-  this.getExercisesDataFromDatabase = async function getExercisesDataFromDatabase() {
-  };
-
-  this.evaluateAnswersData = function evaluateAnswersData() {
-    this.exercises.forEach((exercise) => {
-      exercise.evaluateSingleAnswer();
-    });
-  };
-}
-
-function Exercise(answer) {
-  this.data = {
-    id: answer.id,
-    actualAnswer: answer.actualAnswer,
-    answerWasCorrect: undefined,
-    tekst: undefined,
-    facit: undefined,
-    point: undefined,
-    type: undefined,
-  };
-
-  this.getSingleExerciseDataFromDatabase = function getSingleExerciseDataFromDatabase() {
-  };
-
-  this.evaluateSingleAnswer = function evaluateSingleAnswer() {
-    if (this.data.actualAnswer === this.data.facit) {
-      this.data.answerWasCorrect = true;
-    } else if (this.data.actualAnswer !== this.data.facit) {
-      this.data.answerWasCorrect = false;
-    }
-  };
-}
-
-function ResultPage(exerciseSuite) {
-  this.page = '';
-  this.page += createPageHead();
-  this.page += createPageBody(exerciseSuite);
-
-  this.respondTo = function respondTo(response) {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end(this.page);
-  };
-}
-
-const createPageHead = () => `<!DOCTYPE html>
-  <head>
-  <title>P2 Projekt Resultat Side</title>
-  <link rel='icon' type='image/png' href='./favicon.png' />
-  <meta charset="UTF-8">
-  </head>
-  `;
-
-const createPageBody = (exerciseSuite) => {
-  let pageBody = '';
-  pageBody += '<body>';
-
-  let exerciseNumber = 1;
-  exerciseSuite.exercises.forEach((exercise) => {
-    this.page += `<div class="exercise" id="exercise${exerciseNumber}">\n`;
-    this.page += `<h1>Opgave ${exerciseNumber}</h1>\n`;
-    this.page += '<div class="exerciseBody">\n';
-    this.page += `<p>${exercise.data.tekst}</p>\n`;
-    this.page += '</div>\n';
-
-    this.page += '<div class="answerBody">\n';
-    this.page += '<h2> Svar og facit</h2>\n';
-    if (exercise.data.answerWasCorrect) {
-      this.page += '<p style="background-color: lightgreen">Dit svar var korrekt</p>\n';
-    } else if (!exercise.data.answerWasCorrect) {
-      this.page += '<p style="background-color: lightcoral">Dit svar var forkert</p>\n';
-    }
-    this.page += `<p><b>Korrekte svar:</b> ${exercise.data.facit}</p>\n`;
-    this.page += `<p><b>Dit svar:</b> ${exercise.data.actualAnswer}</p>\n`;
-    this.page += '</div>\n';
-
-    this.page += '<div class="exerciseData">\n';
-    this.page += '<h2>Opgave indsigt</h2>\n';
-    this.page += `<p><b>Emne:</b> ${exercise.data.type}</p>\n`;
-    this.page += `<p><b>Points:</b> ${exercise.data.point}</p>\n`;
-    this.page += '</div>\n';
-    this.page += '</div>\n';
-    exerciseNumber += 1;
-  });
-
-  pageBody += '</body>';
-  return pageBody;
-};
 
 module.exports = handleResultRequest;
