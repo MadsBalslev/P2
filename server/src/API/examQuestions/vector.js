f51f5dd5043b879a6cb331d4c39d8280e898cbee
Simon Andersen <smpa20@student.aau.dk>
2021-04-20 09:11:10 +0200

    Added shit

--- server/src/API/examQuestions/vector.js
+++ server/src/API/examQuestions/vector.js
@@ -31,21 +31,11 @@ function VektorAdditionExercise() {
   this.tegn = '+';
 }
 
-<<<<<<< HEAD
-function getVektorAdditionFacit(vectorA, vectorB) {
-  return math.matrix([
-    [math.subset(vectorA, math.index(0, 0)) + math.subset(vectorB, math.index(0, 0))],
-    [math.subset(vectorA, math.index(1, 0)) + math.subset(vectorB, math.index(1, 0))],
-  ]);
-}
-/* Generates two vectors needed for a substraction exercise and calculates the right answer for the exercise */
-=======
 const getVektorAdditionFacit = (vectorA, vectorB) => math.matrix([
   [math.subset(vectorA, math.index(0, 0)) + math.subset(vectorB, math.index(0, 0))],
   [math.subset(vectorA, math.index(1, 0)) + math.subset(vectorB, math.index(1, 0))],
 ]);
 
->>>>>>> 7e6119b6672fdb579b84f46f0390d0ef5d462200
 function VektorSubtractionExercise() {
   const mathMatrixVectorA = generateRandom2dVector();
   const mathMatrixVectorB = generateRandom2dVector();
@@ -62,25 +52,10 @@ function VektorSubtractionExercise() {
   this.tegn = '-';
 }
 
-<<<<<<< HEAD
-/**
- * This functions subtracts two inputted vectors and returns the correct answer as a vector.
- * @param {math.Matrix} vectorA A vector formatted as a string, necessary for calculating output.
- * @param {math.Matrix} vectorB Another vector formatted as a string, necessary for calculating output.
- * @return {math.Matrix} Returns a formatted vector matrix, calculated by subtracting the two inputted vectors.
- */
-function getVektorSubtractionFacit(vectorA, vectorB) {
-  return math.matrix([
-    [math.subset(vectorA, math.index(0, 0)) - math.subset(vectorB, math.index(0, 0))],
-    [math.subset(vectorA, math.index(1, 0)) - math.subset(vectorB, math.index(1, 0))],
-  ]);
-}
-=======
 const getVektorSubtractionFacit = (vectorA, vectorB) => math.matrix([
   [math.subset(vectorA, math.index(0, 0)) - math.subset(vectorB, math.index(0, 0))],
   [math.subset(vectorA, math.index(1, 0)) - math.subset(vectorB, math.index(1, 0))],
 ]);
->>>>>>> 7e6119b6672fdb579b84f46f0390d0ef5d462200
 
 /* This function generates two vectors needed for a multiplication exercise and calculates the right answer for the exercise */
 function VektorMultiplicationExercise() {
@@ -100,14 +75,7 @@ function VektorMultiplicationExercise() {
   this.tegn = '*';
 }
 
-<<<<<<< HEAD
-/* Function we can call with the parameters of two vectors to caluclate the right answer */
-function getVektorMultiplicationFacit(vectorA, vectorB) {
-  return math.dot(vectorA, vectorB);
-}
-=======
 const getVektorMultiplicationFacit = (vectorA, vectorB) => math.dot(vectorA, vectorB);
->>>>>>> 7e6119b6672fdb579b84f46f0390d0ef5d462200
 
 const numOfTasks = 3;
 
