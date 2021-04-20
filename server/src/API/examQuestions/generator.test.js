14e21a58804cb17a4f062c89450f37efca38e9d9
CasperStaahl <cstahl20@student.aau.dk>
2021-04-20 11:18:20 +0200

    Unit test, refactor, og dokumentation, [DON'T MERGE] (#100)
    * create unit tests for vector.js
    
    * rename functions
    
    * ran npm install
    
    * VektorAdditionExercise is a constructor  now
    
    * refactor vector.js
    
    VektorSubtractionExercise is constructor now
    VektorMultiplicationExercise is constructor now
    
    * unit test generator
    
    * fix error in vector.js
    
    * refactor and unit testing
    
    move createTable.js to deprecated
    move dbjs to depracated
    
    refactor site.html
    
    * refactor
    
    move jsonGen.js to deprecated
    move searchQuestions.js to depracated
    
    refactor generator.js
    refactor app.js
    refactor helper.js
    
    * refactor integral.js, vector.js
    
    * refactor opgaver.js
    
    * fix lint error
    
    * Added documentation
    
    * Added shit
    
    * refactor integral.js
    
    * Documentation added
    
    * document integral.js
    
    * Added comments to site.html
    
    * Update integral.test.js
    
    * run lint
    
    Co-authored-by: Simon Andersen <smpa20@student.aau.dk>
    Co-authored-by: Mightyhaha <74659365+Mightyhaha@users.noreply.github.com>

---
+++ server/src/API/examQuestions/generator.test.js
@@ -0,0 +1,53 @@
+const { generateExcerciseSet } = require('./generator');
+
+const empty = '';
+
+test('generateExerciseSet', () => {
+  const amount = 10;
+  const categories = [
+    'andengradspolynomiumOgLigning',
+    'talOgegnearter',
+    'ligninger',
+    'trigonometri',
+    'funktioner',
+    'geometri',
+    'differebtialregning',
+    'sandsynlighedOgKombinatorik',
+    'statistik',
+    'regression',
+    'vektor2d',
+    'vektorerI3d',
+    'vektorfunktioner',
+    'infinitesimalregning',
+    'integralregning',
+    'funktionerAfToVariable',
+  ];
+
+  const exerciseSet = generateExcerciseSet(categories, amount);
+
+  exerciseSet.forEach((exercise) => {
+    if (errorInExerciseFields(exercise)) {
+      console.info(`error in exercise: ${exercise.txt}`);
+      console.info(`exercise type was: ${exercise.type}`);
+    }
+
+    expect(typeof exercise.facit).toBe('string');
+    expect(typeof exercise.type).toBe('string');
+    expect(typeof exercise.point).toBe('number');
+    expect(typeof exercise.txt).toBe('string');
+    expect(typeof exercise.tegn).toBe('string');
+
+    expect(exercise.txt).not.toBe(empty);
+    expect(exercise.type).not.toBe(empty);
+  });
+});
+
+function errorInExerciseFields(exercise) {
+  return typeof exercise.facit !== 'string'
+    || typeof exercise.type !== 'string'
+    || typeof exercise.point !== 'number'
+    || typeof exercise.txt !== 'string'
+    || typeof exercise.tegn !== 'string'
+    || exercise.txt === empty
+    || exercise.type === empty;
+}
