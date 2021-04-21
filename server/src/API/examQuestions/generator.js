9acd02344d7e45a556448aca5bf04663f4dc7469
Casper Ståhl <cstahl20@student.aau.dk>
2021-04-21 09:10:50 +0200

    fix lint error

--- server/src/API/examQuestions/generator.js
+++ server/src/API/examQuestions/generator.js
@@ -10,7 +10,8 @@ const integrals = require('./integral');
  * Will generate an exerciseset with the given catagories
  * @param {string[]} categories An array containg the catagories of exercises to be generated
  * @param {integer} amount The amount of exercises to be generated in each catagory
- * @return {any[]} The function returns a generated excercise set as an array, based on the two input parameters.
+ * @return {any[]} The function returns a generated excercise set as an array, based on the two
+ * input parameters.
  */
 const generateExcerciseSet = (categories, amount) => {
   const set = [];
