6c6b8ed1847e3aa61efd81ded59b726595a00271
Casper Ståhl <cstahl20@student.aau.dk>
2021-04-23 15:46:55 +0200

    fix lint error/warn

--- src/API/examQuestions/ligninger.js
+++ src/API/examQuestions/ligninger.js
@@ -2,52 +2,53 @@
 const { randNum } = require('../../helper');
 
 const generateVars = () => {
-    const A = randNum(10) + 1; // 5x
-    const B = randNum(35); // 13
-    const minNum = A + B;
-    const C = randNum(40) + minNum;
+  const A = randNum(10) + 1; // 5x
+  const B = randNum(35); // 13
+  const minNum = A + B;
+  const C = randNum(40) + minNum;
 
-    return { A, B, C };
+  return { A, B, C };
 };
 
-function LigningPlusExercise () {
-    const { A, B, C} = generateVars();
-    this.txt = 'Find x i følgende ligning.';
-    this.type = 'ligninger';
-    this.point = 5;
-    this.tegn = '+';
-    this.exerciseVars = { ligning: `${A}x + ${B} = ${C}` };
-    this.facit = LigningFacit(A, B, C, this.tegn);
+function LigningPlusExercise() {
+  const { A, B, C } = generateVars();
+  this.txt = 'Find x i følgende ligning.';
+  this.type = 'ligninger';
+  this.point = 5;
+  this.tegn = '+';
+  this.exerciseVars = { ligning: `${A}x + ${B} = ${C}` };
+  this.facit = LigningFacit(A, B, C, this.tegn);
 }
 
-function LigningMinusExercise () {
-    const { A, B, C } = generateVars();
-    this.txt = 'Find x i følgende ligning.';
-    this.type = 'ligninger';
-    this.point = 5;
-    this.tegn = '-';
-    this.exerciseVars = { ligning: `${A}x - ${B} = ${C}` };
-    this.facit = LigningFacit(A, B, C, this.tegn);
+function LigningMinusExercise() {
+  const { A, B, C } = generateVars();
+  this.txt = 'Find x i følgende ligning.';
+  this.type = 'ligninger';
+  this.point = 5;
+  this.tegn = '-';
+  this.exerciseVars = { ligning: `${A}x - ${B} = ${C}` };
+  this.facit = LigningFacit(A, B, C, this.tegn);
 }
 
 const LigningFacit = (A, B, C, symbol) => {
-    if (symbol === '+') {
-        let answer = (C - B) / A;
-        let ansFixed = answer.toFixed(1);
-        return `${ansFixed}`}
-    else if (symbol === '-') {
-        let answer = (C + B) / A;
-        let ansFixed = answer.toFixed(1);
-        return `${ansFixed}`}
-    
-    return "";
-}
+  if (symbol === '+') {
+    const answer = (C - B) / A;
+    const ansFixed = answer.toFixed(1);
+    return `${ansFixed}`;
+  } else if (symbol === '-') {
+    const answer = (C + B) / A;
+    const ansFixed = answer.toFixed(1);
+    return `${ansFixed}`
+  }
+
+  return '';
+};
 
 const numOfTasks = 2;
 
 module.exports = {
-    LigningPlusExercise,
-    LigningMinusExercise,
-    LigningFacit,
-    numOfTasks,
-}
+  LigningPlusExercise,
+  LigningMinusExercise,
+  LigningFacit,
+  numOfTasks,
+};
