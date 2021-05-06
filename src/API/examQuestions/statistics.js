bc0c99e3b20df94b8a5e3c3ae8e70db694449c16
Mightyhaha <74659365+Mightyhaha@users.noreply.github.com>
2021-05-06 23:49:21 +0200

    Added documentation for statistic.js

--- src/API/examQuestions/statistics.js
+++ src/API/examQuestions/statistics.js
@@ -1,21 +1,31 @@
 const math = require('mathjs');
 const { randNum } = require('../../helper');
 
-
+/**
+ * Generates a array with randomly generated numbers and the size of the array.
+ * @return {array} Returns array with numbers.
+ * @return {variable} Returns the size of the aary
+ */
 const generateArray = () => {
-  const i = randNum(40);
+  const i = randNum(40) + 1;
   numbers = Array.from({length: i}, () => Math.floor(Math.random(99) * i + 1));
 
   return { numbers, i };
 }
-
+/**
+ * Generates the standard deviation
+ * @return {number} Returns the standard deviation.
+ */
 const generateAfgivelse = () => {
   const afgivelse = randNum(15);
 
   return afgivelse;
 }
 
-
+/**
+ * Generates randomly generated numbers, that can´t be 0
+ * @return {variables} Returns the randomly generated numbers.
+ */
 const generateVars = () => {   
   const A = randNum(75) + 1;
   const B = randNum(50) + 1;
@@ -23,10 +33,18 @@ const generateVars = () => {
   return { A, B };
 }
 
+/**
+ * Used for making it possible to sum up entire array, but adding up all numbers
+ * @return {math.Matrix} Returns the accumulator which is +.
+ */
 function add(accumulator, a) {
   return accumulator + a;
 }
 
+/**
+ * Represents a exercise of bionormal confidence-interval
+ * @constructor
+ */
 function binormalConfidenceIntervalExercise () {
   const { A, B } = generateVars();
   this.txt = `En klasse fra 3.g med ${A} elever har svaret på et spørgeskema om de var klar til matematikeksamen og ${B} har stemt ja. Bestem nu sandsynlighedsparameteren og bagefter brug det til at finde 95%-konfidensintervallet`;
@@ -37,6 +55,10 @@ function binormalConfidenceIntervalExercise () {
   this.facit = binormalConfidenceIntervalFacit(A, B);
 }
 
+/**
+ * Represents a exercise of normal confidence-interval
+ * @constructor
+ */
 function normalConfidenceIntervalExercise () {
  const { numbers, i } = generateArray();
  const afgivelse = generateAfgivelse();
@@ -48,7 +70,13 @@ function normalConfidenceIntervalExercise () {
  this.facit = normalConfidenceIntervalFacit(numbers, i, afgivelse);
 }
 
-
+/**
+ * Generates a vector, and puts in a matrix
+ * @param {array} numbers The array of randomly generated numbers
+ * @param {variable} i The size of the array
+ * @param {variable} afgivelse The standard deviation
+ * @return {facit} Returns the interval and correct answer for the exercise
+ */
 const normalConfidenceIntervalFacit = (numbers, i, afgivelse) => {
 let sqrt;
 let confidenceinterval1;
@@ -64,6 +92,12 @@ confidenceinterval2 = (middelværdi + 1.96 * (afgivelse/sqrt)).toFixed(2);
 return `[${confidenceinterval1};${confidenceinterval2}]`;
 }
 
+/**
+ * Generates a vector, and puts in a matrix
+ * @param {variable} A The first randomly generated number
+ * @param {variable} B The second randomly generated number
+ * @return {facit} Returns the interval and correct answer for the exercise
+ */
 const binormalConfidenceIntervalFacit = (A, B) => {
   
   let confidenceinterval1;
