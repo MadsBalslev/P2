948b9d092c9d5608fa4fe7f2a7300fd380dc6602
MadsBalslev <MadsBalslev@users.noreply.github.com>
2021-05-07 12:08:45 +0000

    deploy: 0fd30a379f11930580702120f8ed2699508e886b

---
+++ src/API/examQuestions/ligningertovariabler.test.js
@@ -0,0 +1,22 @@
+const differination = require('./ligningertovariabler.js');
+
+test('partielDifferentiationFacit', () => {
+const A = 4;
+const B = 2;
+const C = 4;
+const D = 2;
+const expression = 'x';
+
+const FacitExpect = '2 * x + 4';
+
+expect(differination.partielDifferentiationFacit(A, B, C, D, expression)).toEqual(FacitExpect);
+});
+
+
+test('rangeFacit', () => {
+const unknowns = 1;
+const c = 9;
+const rangeExpect = 3;
+
+expect(differination.rangeFacit(unknowns, c)).toEqual(rangeExpect);
+});
