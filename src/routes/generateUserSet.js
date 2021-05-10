bf909cd24b89942fad4f6a6a2f9be04ca89441c8
Casper Ståhl <cstahl20@student.aau.dk>
2021-05-10 12:29:55 +0200

    eod

--- src/routes/generateUserSet.js
+++ src/routes/generateUserSet.js
@@ -1,30 +1,5 @@
+const { generateExcerciseSet } = require('../API/examQuestions/generator');
 const helper = require('../helper');
-
-const IDENTITY_MATRIX_23 = [
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-];
 
 function handleGenerateUserSetRequest(request, response) {
   if (request.method === 'POST') {
@@ -37,7 +12,6 @@ function handleGenerateUserSetRequest(request, response) {
 async function handleGenerateUserSetPostRequest(request, response) {
   try {
     const userProfile = await helper.fetchJsonRequestBody(request);
-    console.log(userProfile);
     validateUserProfile(userProfile);
     const userExerciseSet = generateUserExerciseSet(userProfile);
     helper.respondWithJsonObject(userExerciseSet);
@@ -57,9 +31,44 @@ function userProfileIsValid(userProfile) {
 }
 
 function generateUserExerciseSet(userProfile) {
-  IDENTITY_MATRIX_23.forEach((IdentityVector) => {
-    console.log(cosineSimilarity(IdentityVector, userProfile));
+  const identityMatrix23 = [
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  ];
+
+  identityMatrix23.sort((vectorA, vectorB) => {
+    return cosineSimilarity(vectorB, userProfile) - cosineSimilarity(vectorA, userProfile);
   });
+
+  const exerciseSetPart1 = generateExcerciseSet([vectorToExerciseCategory(identityMatrix23[0])], 3);
+  const exerciseSetPart2 = generateExcerciseSet([vectorToExerciseCategory(identityMatrix23[1])], 3);
+  const exerciseSetPart3 = generateExcerciseSet([vectorToExerciseCategory(identityMatrix23[2])], 2);
+  const exerciseSetPart4 = generateExcerciseSet([randomCategory()], 1);
+  const exerciseSetPart5 = generateExcerciseSet([randomCategory()], 1);
+
+  const exerciseSet = exerciseSetPart1.concat(exerciseSetPart2, exerciseSetPart3, exerciseSetPart4, exerciseSetPart5);
+  return exerciseSet;
 }
 
 function cosineSimilarity(vectorA, vectorB) {
@@ -80,6 +89,64 @@ function lengthOfVector(vector) {
     temp += element ** 2;
   });
   return Math.sqrt(temp);
+}
+
+function vectorToExerciseCategory(vector) {
+  switch (vector.toString()) {
+    case '1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0': return 'talOgRegnearter_C';
+    case '0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0': return 'ligninger_C';
+    case '0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0': return 'funktioner_C';
+    case '0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0': return 'trigonometri_C';
+    case '0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0': return 'geometri_C';
+    case '0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0': return 'sandsynlighed_C';
+    case '0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0': return 'andengradspolynomiumOgLigning_B';
+    case '0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0': return 'trigonometri_B';
+    case '0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0': return 'funktioner_B';
+    case '0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0': return 'geometri_B';
+    case '0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0': return 'differentialregning_B';
+    case '0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0': return 'sandsynlighedOgKombinatori_B';
+    case '0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0': return 'statistik_B';
+    case '0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0': return 'regression_B';
+    case '0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0': return 'vektorerI2d_B';
+    case '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0': return 'vektorerI3d_A';
+    case '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0': return 'vektorfunktioner_A';
+    case '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0': return 'trigonometri_A';
+    case '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0': return 'infinitesimalregning_A';
+    case '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0': return 'differentialregning_A';
+    case '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0': return 'integralregning_A';
+    case '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0': return 'funktionerAfToVariable_A';
+    case '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1': return 'statistik_A';
+    default: return '';
+  }
+}
+
+function randomCategory() {
+  switch (helper.randNum(22)) {
+    case 0: return 'talOgRegnearter_C';
+    case 1: return 'ligninger_C';
+    case 2: return 'funktioner_C';
+    case 3: return 'trigonometri_C';
+    case 4: return 'geometri_C';
+    case 5: return 'sandsynlighed_C';
+    case 6: return 'andengradspolynomiumOgLigning_B';
+    case 7: return 'trigonometri_B';
+    case 8: return 'funktioner_B';
+    case 9: return 'geometri_B';
+    case 10: return 'differentialregning_B';
+    case 11: return 'sandsynlighedOgKombinatori_B';
+    case 12: return 'statistik_B';
+    case 13: return 'regression_B';
+    case 14: return 'vektorerI2d_B';
+    case 15: return 'vektorerI3d_A';
+    case 16: return 'vektorfunktioner_A';
+    case 17: return 'trigonometri_A';
+    case 18: return 'infinitesimalregning_A';
+    case 19: return 'differentialregning_A';
+    case 20: return 'integralregning_A';
+    case 21: return 'funktionerAfToVariable_A';
+    case 22: return 'statistik_A';
+    default: return '';
+  }
 }
 
 module.exports = {
