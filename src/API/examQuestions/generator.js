7cf89e93f590250024e5104a07678f24e25229c6
MadsBalslev <MadsBalslev@users.noreply.github.com>
2021-05-10 08:25:18 +0000

    deploy: da05434509e2ddeba060d17b095ccd1ec15dbd6c

--- src/API/examQuestions/generator.js
+++ src/API/examQuestions/generator.js
@@ -7,6 +7,7 @@ const vectors = require('./vector');
 const vector3d = require('./vector3d');
 const integrals = require('./integral');
 const ligninger = require('./ligninger');
+const diffligning = require('./Differentialligninger');
 const funktionerAfToVariable = require('./ligningertovariabler');
 const statistik = require('./statistics');
 
@@ -34,6 +35,9 @@ const generateExcerciseSet = (categories, amount) => {
           break;
         case 'ligninger':
           set.push(generateLigningExercise(amount));
+          break;
+        case 'differentialligning':
+          set.push(generateDiffLigningExercise(amount));
           break;
         case 'funktionerAfToVariable':
           set.push(generateFunktionerAfToVariableExercise(amount));
@@ -119,6 +123,10 @@ const generateIntegralExercise = () => {
   return exercise;
 };
 
+/**
+ * This function randomly generates an equation exercise and returns it.
+ * @return {object} Returns the generated exercise object.
+ */
 const generateLigningExercise = () => {
   let exercise;
   const rand = randNum(ligninger.numOfTasks);
@@ -128,6 +136,29 @@ const generateLigningExercise = () => {
       break;
     case 2:
       exercise = new ligninger.LigningMinusExercise();
+      break;
+    default:
+      break;
+  }
+  return exercise;
+};
+
+/**
+ * This function randomly generates a differential equation exercise and returns it.
+ * @return {object} Returns the generated exercise object.
+ */
+const generateDiffLigningExercise = () => {
+  let exercise;
+  const rand = randNum(diffligning.numOfTasks);
+  switch(rand) {
+    case 1:
+      exercise = new diffligning.DifferentialLigningExercise();
+      break;
+    case 2:
+      exercise = new diffligning.DifferentialLigningExercise2();
+      break;
+    case 3:
+      exercise = new diffligning.DifferentialLigningExercise3();
       break;
     default:
       break;
