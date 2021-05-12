041a9dcd036dd5883033cd05e7c6b855debf872e
Patrick Bertelsen <pberte20@users.noreply.github.com>
2021-05-12 13:34:05 +0200

    Added test for sinus and tangens exercise

--- src/API/examQuestions/trigonometri.test.js
+++ src/API/examQuestions/trigonometri.test.js
@@ -1,3 +1,15 @@
 const trigonometri = require('./trigonometri.js');
 
-test()
+test('sinusFacit', () => {
+  const A = 4;
+  const B = 3;
+
+  expect(trigonometri.sinusFacit( A, B )).toEqual('-0.865')
+});
+
+test ('TangensExercise', () => {
+  const A = 180
+
+  const tangensExpect = '1.34';
+  expect(trigonometri.tangensFacit(A)).toEqual(tangensExpect);
+});
