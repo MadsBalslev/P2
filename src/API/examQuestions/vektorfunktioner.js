61ddee5bba29c0c93b68e5920c8dfa4dc41aa143
Lucian Ngando <Lucian_live@outlook.dk>
2021-05-13 13:29:31 +0200

    click

--- src/API/examQuestions/vektorfunktioner.js
+++ src/API/examQuestions/vektorfunktioner.js
@@ -2,7 +2,7 @@ const math = require('mathjs');
 const { randNum } = require('../../helper');
 
 /**
- * Variable declarations
+ * Variable declarations randomly
  */
 const t = randNum(1);
 const x = randNum(10) + 2;
@@ -11,12 +11,12 @@ const g = randNum(4) + 4;
 const h = randNum(4) + 5;
 
 /**
- * Represents 
+ * Represents a vectorfunction exercise
  */
 function VektorFunctionExercise() {
   this.txt = `Bestem koordinaterne til parameter­frem­stil­lingens dobbeltpunkter. t = ${t} i (x,y) = (${x},${y})`;
   this.exerciseVars = {
-    vectorfunction1: `overrightarrow${r(t)}=(x(t),y(t))=(${x}*cos(${t}),${y}-${t}-${g}*sin(${h}*${t}))`,
+    vectorfunction1: `\\vec{r(t)}=(x(t),y(t))=(${x}*cos(${t}),${y}-${t}-${g}*sin(${h}*${t}))`,
   };
   this.type = 'vektorfunktioner';
   this.facit = VektorFunctionExerciseFacit();
@@ -24,12 +24,16 @@ function VektorFunctionExercise() {
   this.tegn = '*';
 }
 
+/**
+ * Calculate facit and returning a string
+ * @return facit
+ */
 const VektorFunctionExerciseFacit = () => {
   const xt1 = x * math.cos(t);
   const yt1 = y - t - g * (math.sin(h * t));
   const xt2 = xt1 * t - (2 * Math.pi) + y * (math.sin(h * t));
   const yt2 = '0';
-  const facit = `$overrightarrow{r(t_1)}=$overrightarrow{r(t_2)}=(x(t_1),y(t_1))=
+  const facit = `\\vec{r(t_1)}=\\vec{r(t_2)}=(x(t_1),y(t_1))=
   (x(t_2),y(t_2))=(${xt1},${yt1},${xt2},${yt2})`;
   return facit;
 };
