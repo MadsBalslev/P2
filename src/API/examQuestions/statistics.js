7e2cbdf793ab4eabaebf0b471246a40f7c489674
CasperStaahl <CasperStaahl@users.noreply.github.com>
2021-05-14 10:58:36 +0000

    deploy: b00e55fdb616cf618ffd473bd5cc41e87c490a95

--- src/API/examQuestions/statistics.js
+++ src/API/examQuestions/statistics.js
@@ -42,17 +42,23 @@ const add = (accumulator, a) => accumulator + a;
  * @constructor
  */
 function BinormalConfidenceIntervalExercise() {
-  const { A, B } = generateVars();
+  let antalElever;
+  let antalJa;
+  while (binormalConfidenceIntervalFacit(antalElever, antalJa) === '[NaN;NaN]') {
+    const temp = generateVars();
+    antalElever = temp.A;
+    antalJa = temp.B;
+  }
 
   // eslint-disable-next-line max-len
-  this.txt = `En klasse med ${A} elever har svaret på et spørgeskema og ${B} har stemt ja på spørgeskemaet. Bestem nu sandsynlighedsparameteren og bagefter brug det til at finde 95%-konfidensintervallet`;
+  this.txt = `En klasse med ${antalElever} elever har svaret på et spørgeskema og ${antalJa} har stemt ja på spørgeskemaet. Bestem nu sandsynlighedsparameteren og bagefter brug det til at finde 95%-konfidensintervallet`;
   this.type = 'statistik';
   this.point = 5;
   this.tegn = '';
   this.exerciseVars = {
     ligning: '\\hat{p} - 1.96 * \\sqrt{\\hat{p}*(1-\\hat{p})/n} ; \\hat{p} + 1.96* \\sqrt{\\hat{p}*(1-\\hat{p})/n}',
   };
-  this.facit = binormalConfidenceIntervalFacit(A, B);
+  this.facit = binormalConfidenceIntervalFacit(antalElever, antalJa);
 }
 
 /**
